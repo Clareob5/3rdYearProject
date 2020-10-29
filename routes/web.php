a59b474ee2b15da36d02ca91450b246e92f8e355
vagrant <vagrant@homestead>
2020-10-29 01:08:57 +0000

    Test commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+# @Date:   2020-10-19T20:17:47+01:00
+# @Last modified time: 2020-10-19T20:44:43+01:00
+
+
+
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('reelsandmeals', function () {
+    return '<h1>Reels and Meals</h1>';
+});
