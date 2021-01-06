762c1b98b50a47401b80576da7469037d548983f
vagrant <vagrant@homestead>
2021-01-06 22:47:35 +0000

    Creating an APU pt.1

---
+++ app/Http/Controllers/API/MovieController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Movie;
+
+class MovieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $movies = Movie::all();
+
+        //an array of properties, takes in an array and status code
+        return response()->json([
+          'status' => 'success',
+          'data' => $movies
+        ], 200); //status code 200 (success status)
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $movie = Movie::find($id);
+
+        if($movie === null) {
+          $statusMsg = "Movie not found!";#
+          $statusCode = 404;
+
+        }
+        else {
+          $statusMsg = "Success!";#
+          $statusCode = 200;
+
+        }
+
+         return response()->json([
+           'status' => $statusMsg,
+           'data' => $movie
+         ], $statusCode);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\dummyAPI;
+use App\Http\Controllers\API\MovieController as APIMovieController;
+// use App\Http\Controllers\dummyAPI;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +20,19 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("data",[dummyAPI::class,'getData']);
+// Route::get("data",[dummyAPI::class,'getData']);
+
+//creating a route group in middleware therefore we dont need to define middleware after each route
+Route::middleware('api')->group(function() {
+
+  //GET /api/movies - display all boooks
+  Route::get('/movies', [APIMovieController::class, 'index'])->middleware('api');
+
+  //GET /api/movies/$id - display a specific movie
+  Route::get('/movies/{id}', [APIMovieController::class, 'show'])->middleware('api');
+
+  //POST /api/movies - add new movie to db
+  //PUT /api/movies/$id - edit existing movies
+  //DELTE /api/movies/$id - selete existing movie
+
+});
