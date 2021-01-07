de7a785221028f7732b670a47ec26c4bd0db616e
Clareob5 <clareobrien1998@gmail.com>
2021-01-07 01:32:32 +0000

    Bootstrapping
    Created a guest homepage layout, added images and a new css file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,27 +1,77 @@
 @extends('layouts.guest')
 
 @section('content')
-<div class="container">
+<div class="container background-dark">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Welcome</div>
-
-                <div class="card-body">
+        <div class="col-md-12">
+            <div class="card header">
+                <img src="assets/img/headerimg2.jpg" class="img-fluid" alt="...">
+            </div>
+            <section>
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
+                    <div>
+                      <h3>Popular Movies</h3>
+                    </div>
+                    <div class="row">
+                            <div class="col-md-2 active">
+                                <div class="card my_card">
+                                    <img class="card-img-top img-fluid" src="assets/img/image.jpg" height="300" alt="Card image cap">
+                                    <div class="card-body card_padding">
+                                        <h4 class="card-title title_size">Annabelle</h4>
 
-                    Welcome to Reels and Meals!
+                                    </div>
+                                </div>
+                            </div>
+                            <div class=" col-md-2">
+                                <div class="card my_card">
+                                    <img class="card-img-top img-fluid" src="assets/img/image2.jpg" alt="Card image cap">
+                                    <div class="card-body card_padding">
+                                        <h4 class="card-title title_size">IO</h4>
 
-                  </br>
-                  </br>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class=" col-md-2">
+                                <div class="card my_card">
+                                    <img class="card-img-top img-fluid" src="assets/img/image3.jpg" alt="Card image cap">
+                                    <div class="card-body card_padding">
+                                        <h4 class="card-title title_size">The Last Jedi</h4>
 
-                  Read more <a href="{{ route('about') }}"> About Us</a>
-                  <!-- link goes to about us page using about route -->
-                </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-2">
+                                <div class="card my_card">
+                                    <img class="card-img-top img-fluid" src="assets/img/image4.jpg" alt="Card image cap">
+                                    <div class="card-body card_padding">
+                                        <h4 class="card-title title_size">The Equalizer</h4>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-2">
+                                <div class="card my_card">
+                                    <img class="card-img-top img-fluid" src="assets/img/image5.jpeg" alt="Card image cap">
+                                    <div class="card-body card_padding">
+                                        <h4 class="card-title title_size">Insidious</h4>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-2">
+                                <div class="card my_card">
+                                    <img class="card-img-top img-fluid" src="assets/img/image6.jpeg" alt="Card image cap">
+                                    <div class="card-body card_padding">
+                                        <h4 class="card-title title_size">The Dark Knight</h4>
+
+                                    </div>
+                                </div>
+                            </div>
+                      </section>
             </div>
         </div>
     </div>
